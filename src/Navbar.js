4bbe97d1cf823ad854d79a1a9567ce429b2a7d55
Q. Price <priceqp@gmail.com>
2019-08-05 14:26:35 -0500

    Construct Navbar using material ui components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+
+};
 
 class Navbar extends React.Component {
   render() {
+    const { classes } = this.props
     return (
-      <div>
-        <h1>Navbar is here!</h1>
+      <div className={classes.root}>
+        <AppBar position='static' color='primary'>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color='inherit'>
+              <span>🇯🇲</span>
+            </IconButton>
+            <Typography className={classes.title} variant='h6' color='inherit'>
+              App Title
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
