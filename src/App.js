6b2421691039c2c4d845e56c8ad604a26f8bfcf1
Q. Price <priceqp@gmail.com>
2019-08-05 17:32:46 -0500

    Create PageContent component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Form from './Form';
 import Navbar from './Navbar';
+import PageContent from './PageContent';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <PageContent>
       <Navbar />
       <Form />
-    </>
+    </PageContent>
   );
 }
 
