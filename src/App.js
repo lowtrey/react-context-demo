b067c8ebca4d47f0f6f59db152c010e6b6d7ac6d
Q. Price <priceqp@gmail.com>
2019-08-06 18:14:34 -0500

    Add ability to change laguage of variables using context

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Form from './Form';
 import Navbar from './Navbar';
 import PageContent from './PageContent';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/LanguageContext';
 
 function App() {
   return (
     <ThemeProvider>
-      <PageContent>
-        <Navbar />
-        <Form />
-      </PageContent>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
