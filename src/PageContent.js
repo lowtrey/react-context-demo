c4ca046705483c467ee99588b3c51df183c081e3
Q. Price <priceqp@gmail.com>
2019-08-06 16:55:43 -0500

    Tie context into PageContent component

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ThemeContext } from './contexts/ThemeContext';
 
 class PageContent extends React.Component {
+  static contextType = ThemeContext;
   render() {
+    const { isDarkMode } = this.context;
     const styles = {
-      backgroundColor: 'white',
+      backgroundColor: isDarkMode ? 'black' : 'white',
       height: '100vh',
       width: '100vw'
     };
