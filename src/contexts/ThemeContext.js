96454512078799c53bc59d1c5b336c27188024a2
Q. Price <priceqp@gmail.com>
2019-08-06 17:39:36 -0500

    Create method to toggle dark mode

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,10 +6,14 @@ export class ThemeProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = { isDarkMode: true }
+    this.toggleTheme = this.toggleTheme.bind(this);
+  };
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
   }
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state, tastesLikeChicken: true }}>
+      <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
         {this.props.children}
       </ThemeContext.Provider>
     )
